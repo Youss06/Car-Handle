1160ad61ad73c685ef8f7e7e6200e82d86bea92f
Youss06 <oumar.youssouf0605@gmail.com>
2017-10-27 16:08:35 +0200

    Add home

--- views/indexVue.php
+++ views/indexVue.php
@@ -1,9 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 <?php
 // Include Header
 include("template/header.php");
 
+?>
 
+<div  class="col-lg-4">
+    <div style="background-color:rgba(250,250,250,0.4)" class="card">
+      <div class="card-block">
+        <h3 class="card-title"></h3>
+        <p class="card-text"></p>
+        <p class="card-text"></p>
+        <a href="" class="btn btn-primary">Détails</a>
+        <a href="" class="btn btn-primary tonbou">Créer</a>
+        <a href="" class="btn btn-primary tonbou">Modifier</a>
+        <a href="" class="btn btn-primary tonbou">Supprimer</a>
+      </div>
+    </div>
+  </div>
 
+<?php
 // Include Footer
 include("template/footer.php");
+
  ?>
+  </body>
+</html>
+
+<?php
