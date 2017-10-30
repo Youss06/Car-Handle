79fd1c222380386ac4201d5d4d25d95c559a7244
Youss06 <oumar.youssouf0605@gmail.com>
2017-10-30 11:23:47 +0100

    Add vonstructor and hydrate

--- entities/Vehicule.php
+++ entities/Vehicule.php
@@ -7,10 +7,19 @@ Protected $color;
 Protected $years;
 
   // constructeur
-
+  public function __construct(array $data){
+      $this->hydrate($data);
+    }
 
   // Hydrateur
-
-
+  public function hydrate(array $data)
+    {
+      foreach ($data as $key => $value) {
+        $method = 'set' . ucfirst($key);
+        if (method_exists($this,$method)) {
+        $this->$method($value);
+        }
+      }
+}
 
 }
