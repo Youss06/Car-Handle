e00dd6edd1025734158b9f7d1e019d6e8cb69791
Youss06 <oumar.youssouf0605@gmail.com>
2017-10-30 12:19:06 +0100

    Add param

--- model/car_manager.php
+++ model/car_manager.php
@@ -5,7 +5,15 @@ class CarManager {
 
   // constructeur
 
-  public function add(chat $c ){
+  public function add(vehicule $recovery ){
     $q = $this->getBdd()->prepare('INSERT INTO vehicule(model, mark, color, years)
     VALUES(:model, :mark, :color, :years)');
+
+    // ICI Rappeler les Getters (getNom...)
+    $q->bindValue(':model', $recovery->getModel());
+    $q->bindValue(':mark', $recovery->getMark(), PDO::PARAM_INT);
+    $q->bindValue(':color', $recovery->getColor(), PDO::PARAM_STR);
+    $q->bindValue(':years', $recovery->getYears(), PDO::PARAM_STR);
+
+    $q->execute();
 }
